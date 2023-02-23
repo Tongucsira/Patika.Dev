45162db1cc595815bc4919bc6e58f64dcb11f4af
Tonguç Şira <siratonguc@gmail.com>
2023-02-23 04:46:41 +0300

    Grocery Store v1

---
+++ src/GroceryStore.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+/**
+ * @author Tonguç Şira
+ */
+public class GroceryStore {
+
+    public static void myGroceryStore(){
+        Scanner input = new Scanner(System.in);
+        double armutKgFiyat = 2.14;
+        double elmaKgFiyat = 3.67;
+        double domatesKgFiyat = 1.11;
+        double muzKgFiyat = 0.95;
+        double patlicanKgFiyat = 5.00;
+        double toplamTutar = 0.0;
+
+        System.out.println("Manavdan aldığınız ürünleri ve kilogramlarını giriniz:");
+        System.out.print("Armut (kg): ");
+        double armutKg = input.nextDouble();
+        toplamTutar += armutKg * armutKgFiyat;
+
+        System.out.print("Elma (kg): ");
+        double elmaKg = input.nextDouble();
+        toplamTutar += elmaKg * elmaKgFiyat;
+
+        System.out.print("Domates (kg): ");
+        double domatesKg = input.nextDouble();
+        toplamTutar += domatesKg * domatesKgFiyat;
+
+        System.out.print("Muz (kg): ");
+        double muzKg = input.nextDouble();
+        toplamTutar += muzKg * muzKgFiyat;
+
+        System.out.print("Patlıcan (kg): ");
+        double patlicanKg = input.nextDouble();
+        toplamTutar += patlicanKg * patlicanKgFiyat;
+
+        System.out.printf("Toplam Tutar: %.2f TL", toplamTutar);
+    }
+}
