2a9f00d989de45c6c6f3540d55e6cac3c470b45c
Tonguç Şira <siratonguc@gmail.com>
2023-02-24 07:31:11 +0300

    Number Sorting

---
+++ src/NumberSorting.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+/**
+ * @author Tonguç Şira
+ */
+public class NumberSorting {
+
+
+    public static void myNumberSorting() {
+
+        int a, b, c, temp;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("a değerini giriniz : ");
+        a = input.nextInt();
+
+        System.out.print("b değerini giriniz : ");
+        b = input.nextInt();
+
+        System.out.print("c değerini giriniz : ");
+        c = input.nextInt();
+
+        if ((a < b) && (a < c)) {
+            if (b < c) {
+                System.out.println("a < b < c");
+            } else {
+                System.out.println("a < c < b");
+            }
+        } else if ((b < a) && (b < c)) {
+            if (a < c) {
+                System.out.println("b < a < c");
+            } else {
+                System.out.println("b < c < a");
+            }
+        } else if ((c < a) && (c < b)) {
+            if (a < b) {
+                System.out.println("c < a < b");
+            } else {
+                System.out.println("c < b < a");
+            }
+        }
+    }
+}
