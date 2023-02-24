6df4a1af204f973ab1abc4e0220fd1653f364928
Tonguç Şira <siratonguc@gmail.com>
2023-02-24 09:07:10 +0300

    Chinesse Zodiac Calculator v1

---
+++ src/ChineeseZodiac.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * @author Tonguç Şira
+ */
+public class ChineeseZodiac {
+
+    public static void myChineeseZodiacCalculator(){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Doğum yılınızı girin: ");
+        int year = input.nextInt();
+
+        switch (year % 12) {
+            case 0: System.out.println("Çin Zodyağı Burcunuz : Maymun"); break;
+            case 1: System.out.println("Çin Zodyağı Burcunuz : Horoz"); break;
+            case 2: System.out.println("Çin Zodyağı Burcunuz : Köpek"); break;
+            case 3: System.out.println("Çin Zodyağı Burcunuz : Domuz"); break;
+            case 4: System.out.println("Çin Zodyağı Burcunuz : Fare"); break;
+            case 5: System.out.println("Çin Zodyağı Burcunuz : Öküz"); break;
+            case 6: System.out.println("Çin Zodyağı Burcunuz : Kaplan"); break;
+            case 7: System.out.println("Çin Zodyağı Burcunuz : Tavşan"); break;
+            case 8: System.out.println("Çin Zodyağı Burcunuz : Ejderha"); break;
+            case 9: System.out.println("Çin Zodyağı Burcunuz : Yılan"); break;
+            case 10: System.out.println("Çin Zodyağı Burcunuz : At"); break;
+            case 11: System.out.println("Çin Zodyağı Burcunuz : Koyun"); break;
+        }
+    }
+}
