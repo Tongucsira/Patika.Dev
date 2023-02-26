7b600adf4d30151a84064984c4723b38bc3b60a9
Tonguç Şira <siratonguc@gmail.com>
2023-02-26 23:51:01 +0300

    Odd Numbers Calculator v1

---
+++ src/OddNumbersCalculator.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * @author Tonguç Şira
+ */
+public class OddNumbersCalculator {
+    public static void myOddCalculator() {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        int total = 0;
+        int counter = 0;
+
+        do {
+            System.out.print("Bir sayı girin: ");
+            n = sc.nextInt();
+            if (n % 2 == 0 && n % 4 ==0) {
+                total += n;
+            }
+        }
+        while (n % 2 == 0);
+
+        System.out.println("Girilen sayıların toplamı: " + total);
+    }
+}
+
