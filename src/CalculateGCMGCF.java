7deb32569e5a62a78bb76a4466581d4e2c77c6d2
Tonguç Şira <siratonguc@gmail.com>
2023-03-01 08:27:55 +0300

    GCM GCF Calculator v1

--- src/CalculateGCMGCF.java
+++ src/CalculateGCMGCF.java
@@ -9,41 +9,24 @@ public class CalculateGCMGCF {
 
         Scanner input = new Scanner(System.in);
 
-        // Kullanıcıdan iki sayı iste
-
         System.out.print("1. sayıyı giriniz: ");
         int num1 = input.nextInt();
 
         System.out.print("2. sayıyı giriniz: ");
         int num2 = input.nextInt();
 
-        // Ebob değerini hesapla
-        int ebob = findEbob(num1, num2);
-
-        // Ekok değerini hesapla
-        int ekok = findEkok(num1, num2, ebob);
-
-        System.out.println("Girilen sayıların ebob değeri: " + ebob);
-        System.out.println("Girilen sayıların ekok değeri: " + ekok);
-    }
-
-    // Ebob değerini hesaplamak için fonksiyon
-    public static int findEbob(int num1, int num2) {
         int ebob = 1;
-
-        for (int i = 1; i <= num1 && i <= num2; i++) {
+        int i = 1;
+        while (i <= num1 && i <= num2) {
             if (num1 % i == 0 && num2 % i == 0) {
                 ebob = i;
             }
+            i++;
         }
 
-        return ebob;
-    }
-
-    // Ekok değerini hesaplamak için fonksiyon
-    public static int findEkok(int num1, int num2, int ebob) {
         int ekok = (num1 * num2) / ebob;
 
-        return ekok;
+        System.out.println("Girilen sayıların ebob değeri: " + ebob);
+        System.out.println("Girilen sayıların ekok değeri: " + ekok);
     }
 }
