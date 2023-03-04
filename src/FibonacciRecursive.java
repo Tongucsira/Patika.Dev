2778d7bf9e774eb7fb75713aaa573169f5ebc744
Tonguç Şira <siratonguc@gmail.com>
2023-03-04 08:52:07 +0300

    Calculate Fibonacci with Recursive Methods v1

---
+++ src/FibonacciRecursive.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * @author Tonguç Şira
+ */
+public class FibonacciRecursive {
+
+    public static void myFibonacciRecursive() {
+
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Kaç adet Fibonacci sayısı istiyorsunuz? : ");
+        int n = myScanner.nextInt();
+
+
+        CalculateFibonacciWithRecursiveAndPrint(n);
+    }
+
+    static void CalculateFibonacciWithRecursiveAndPrint(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    private static int fibonacci(int n) {
+         if (n <= 2) {
+            return 1;
+        }
+         else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
