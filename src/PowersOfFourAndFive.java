ade9647ad00a0db84d6cb04df8873d1c1532aa07
Tonguç Şira <siratonguc@gmail.com>
2023-03-15 18:38:25 +0300

    Revised v1

--- src/PowersOfFourAndFive.java
+++ src/PowersOfFourAndFive.java
@@ -9,14 +9,20 @@ public class PowersOfFourAndFive {
 
         Scanner sc = new Scanner(System.in);
 
+        // Kullanıcıdan sayı alıyoruz.
+
         int n;
         System.out.print("Bir sayı giriniz : ");
         n = sc.nextInt();
+
+        // Kullanıcı negatif sayı girdiğinde uyarı veriyoruz.
 
         if (n < 0) {
             System.out.print("Lütfen pozitif bir sayı giriniz : ");
             n = sc.nextInt();
         }
+
+        // 4 ve 5'in kuvvetlerini yazdırıyoruz.
 
         System.out.println("4'ün kuvvetleri");
         for (int i = 1; i <= n; i *= 4) {
